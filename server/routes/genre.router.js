8ced3ff3f8afc27f7a56c9a39f6a602ff2ad52f4
Ryan Maas <rdmjobs@live.com>
2021-06-19 12:06:19 -0500

    Initial commit, added /genre GET server-side.

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,8 +3,19 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
-});
+  console.log('In /genre GET');
+  // ⬇ Declaring SQL commands to send to DB: 
+  const query = `SELECT * from "genres" ORDER BY "name" ASC;`;
+  pool.query(query)
+    .then(result => {
+      console.log('In /genre GET, result:', result.rows);
+      // ⬇ Sends back the results in an object, we always want rows:
+      res.send(result.rows);
+    }) // End .then
+    .catch(error => {
+      console.error('In /genre GET, error:', error);
+      res.sendStatus(500); // Server error. 
+    }); // End .catch
+}); // End /genre GET
 
 module.exports = router;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,29 +4,31 @@ import './MovieList.css'
 
 function MovieList() {
 
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
+  const dispatch = useDispatch();
+  const movies = useSelector(store => store.movies);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
+  useEffect(() => {
+    dispatch({ type: 'FETCH_MOVIES' });
+  }, []);
 
-    return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
+  return (
+    <main>
+      
+      <h1>MovieList</h1>
 
-    );
+      <section className="movies">
+        {movies.map(movie => {
+          return (
+            <div key={movie.id} >
+              <h3>{movie.title}</h3>
+              <img src={movie.poster} alt={movie.title} />
+            </div>
+          );
+        })}
+      </section>
+
+    </main>
+  );
 }
 
 export default MovieList;
