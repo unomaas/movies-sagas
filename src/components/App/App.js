aa1204096ca437a6577b15da0f424d07d9805e2c
Ryan Maas <rdmjobs@live.com>
2021-06-19 13:19:01 -0500

    Added header.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,24 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import Header from '../Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
+    <Router>
+      <div className="App">
+        <Header />
+
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
+
         {/* Details page */}
 
         {/* Add Movie page */}
-      </Router>
-    </div>
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Header.css';
+import { Link } from 'react-router-dom';
+
+
+export default function Header() {
+  return (
+    <div>
+      <header className='header'>
+        <img className='header-image' src="./images/GenLogoWEmbedded.svg" height="50"></img>
+        <nav>
+          <ul className="Header-nav">
+            <li>
+              <Link className="link" to='/'>Home</Link>&nbsp;
+            </li>
+            <li>
+              <Link className="link" to='/favorites'>Add New Movie</Link>&nbsp;
+            </li>
+          </ul>
+        </nav>
+        <div className="header-main">
+          <p className='header-title'>The Movies Saga</p>
+          {/* <p className="header-subtitle"> v1.0</p> */}
+        </div>
+
+      </header>
+    </div>
+  )
+}
